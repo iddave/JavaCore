6a8c83f640738ec0552e2e433a5becc68b86ad12
David <laliev2001@gmail.com>
2023-03-20 19:28:23 +0300

    chapter04

---
+++ src/main/java/com/Laliev/javacore/chapter04/Modulus.java
@@ -0,0 +1,11 @@
+package com.Laliev.javacore.chapter04;
+
+public class Modulus {
+    public static void main(String[] args) {
+        System.out.println("Операция деления по модулю % .");
+        int x = 42;
+        double y = 42.25;
+        System.out.println("x mod 10 = " + x % 10); // остаток от деления 42/10
+        System.out.println("y mod 10 = " + y % 10); // остаток от деления 42.25/10
+    }
+}
